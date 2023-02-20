b3e4969119bd20ef47065db1efd3d49a03bf3552
adewale-samson <mascot4sure@gmail.com>
2023-02-20 13:54:03 +0100

    hero section of the home page developed

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = '82a3c2e62863d60a0e3adcd6b1716efb'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`
+}
+
+export default requests
